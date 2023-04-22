40bbb4c6483cb0d8d3131b773b837caca5704f7a
Md Abir Hossain <hossain.mdabir@gmail.com>
2023-04-23 04:19:58 +0600

    ListView and DetailsView of that ListView is added

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -23,9 +23,13 @@ struct ContentView: View {
                         .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
                     
                     ForEach(animals) { animal in
-                        AnimalListItemView(animal: animal)
-                    }
+                        NavigationLink(destination: AnimalDetailView(animal: animal)) {
+                            AnimalListItemView(animal: animal)
+                        } //: LINK
+                    } //: LOOP
                 } //: LIST
+                .navigationTitle("Africe")
+                .navigationBarTitleDisplayMode(.large)
             } //: NAVIGATION
         }
     }
